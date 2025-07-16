135f0eb294e92d2393fcc1376826e5224958cf1d
sulaiman shabbir <sulaimanshabbir31@gmail.com>
2025-07-16 21:30:43 +0500

    feat: add Message and Fragment models to Prisma schema, implement message handling in code agent function, and update TRPC router for message management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,26 @@
 
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const Page = () => {
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({}));
   return (
     <div className="p-4 max-w-7xl mx-auto">
       <Button
         onClick={() =>
-          invoke.mutate({
-            text: "Create a kanban drag and drop board use react-beautiful-dnd",
+          createMessage.mutate({
+            value: "Create a landing yellow color page",
           })
         }
       >
         Invoke Background Job
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,24 +5,30 @@ import {
   createNetwork,
   createTool,
   openai,
+  type Tool,
 } from "@inngest/agent-kit";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import { z } from "zod";
 import { PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+interface AgentState {
+  summary: string;
+  files: { [path: string]: string };
+}
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("vibe-nextjs-sulaiman-31");
       return sandbox.sandboxId;
     });
 
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "Code-agent",
-      description:
-        "An agent for writing code in a sandboxed Next.js environment",
+      description: "An expert coding agent",
       system: PROMPT,
       model: openai({
         model: "gpt-4.1",
@@ -74,8 +80,11 @@ export const helloWorld = inngest.createFunction(
               })
             ),
           }),
-          handler: async ({ files }, { step, network }) => {
-            const newFiles = step?.run("createOrUpdateFile", async () => {
+          handler: async (
+            { files },
+            { step, network }: Tool.Options<AgentState>
+          ) => {
+            const newFiles = await step?.run("createOrUpdateFile", async () => {
               try {
                 const updatedFiles = network.state.data.files || {};
                 const sandbox = await getSandbox(sandboxId);
@@ -83,16 +92,14 @@ export const helloWorld = inngest.createFunction(
                   await sandbox.files.write(file.path, file.content);
                   updatedFiles[file.path] = file.content;
                 }
-
                 return updatedFiles;
               } catch (error) {
                 console.error(`Error creating/updating files: ${error}`);
               }
-
-              if (typeof newFiles === "object") {
-                network.state.data.files = newFiles;
-              }
             });
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
+            }
           },
         }),
 
@@ -138,7 +145,7 @@ export const helloWorld = inngest.createFunction(
       },
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name: "coding-agent-network",
       agents: [codeAgent],
       maxIter: 15,
@@ -153,11 +160,42 @@ export const helloWorld = inngest.createFunction(
 
     const result = await network.run(event.data.value);
 
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length === 0;
+
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
       const host = sandbox.getHost(3000);
 
       return `https://${host}`;
+    });
+
+    await step.run("save-result", async () => {
+      if (isError) {
+        return await prisma.message.create({
+          data: {
+            content: "Something went wrong while running the code agent",
+            role: "ASSISTANT",
+            type: "ERROR",
+          },
+        });
+      }
+
+      return await prisma.message.create({
+        data: {
+          content: result.state.data.summary,
+          role: "ASSISTANT",
+          type: "RESULT",
+          fragment: {
+            create: {
+              sandboxUrl: sandboxUrl,
+              title: "Fragment",
+              files: result.state.data.files || {},
+            },
+          },
+        },
+      });
     });
 
     return {
