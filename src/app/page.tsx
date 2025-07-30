b843f847330c4337de9a3d24fb407a0f18bc86f1
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2025-07-30 22:28:05 +0500

    feat: refactor project creation UI to use dynamic input and improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,44 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
-import React from "react";
+import { useRouter } from "next/navigation";
+
+import React, { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
   const trpc = useTRPC();
+  const router = useRouter();
+  const [value, setValue] = useState("");
 
   const createProject = useMutation(
     trpc.projects.create.mutationOptions({
       onError: (error) => {
         toast.error(`Error creating project: ${error.message}`);
       },
+      onSuccess: (data) => {
+        toast.success("Project created successfully!");
+        router.push(`/projects/${data.id}`);
+      },
     })
   );
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <Button
-        onClick={() =>
-          createProject.mutate({
-            value: "Create a landing yellow color page",
-          })
-        }
-      >
-        Invoke Background Job
-      </Button>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex flex-col items-center gap-y-4 justify-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          onClick={() =>
+            createProject.mutate({
+              value,
+            })
+          }
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
